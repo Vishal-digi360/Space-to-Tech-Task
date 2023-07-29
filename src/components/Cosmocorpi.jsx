dc169beca8500d22defcdf6d42fa4c9c0e048db4
Vishal-digi360 <112608428+Vishal-digi360@users.noreply.github.com>
2023-07-29 12:39:39 +0530

    okkk

--- src/components/Cosmocorpi.jsx
+++ src/components/Cosmocorpi.jsx
@@ -5,7 +5,7 @@ const Cosmocorpi = () => {
   return (
     <>
     <div className='relative'>
-    <div className='px-3 sm:flex sm:flex-wrap items-center flex flex-col justify-center gap-10 mt-20  bg-opacity-60 opacity-60'>
+    <div className='px-3 sm:flex sm:flex-wrap items-center sm:flex-row flex flex-col justify-center gap-10 mt-20  bg-opacity-60 opacity-60'>
     <img src="https://archiviomariorusso.it/admin/wp-content/uploads/2020/10/cosmocorpi_1.jpg" alt="" data-aos="zoom-in" data-aos-duration="1500" className='w-80'/>
     <img src="https://archiviomariorusso.it/admin/wp-content/uploads/2020/10/cosmocorpi_2.jpg" alt=""  data-aos="zoom-in" data-aos-duration="2000" className='w-80' />
     <img src="https://archiviomariorusso.it/admin/wp-content/uploads/2020/10/cosmocorpi_3.jpg" alt="" data-aos="zoom-in"  data-aos-duration="2500" className='w-80'/>
