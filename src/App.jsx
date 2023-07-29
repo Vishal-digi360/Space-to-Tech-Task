11e28fb257a2dbd7d29d490f4c9910e82af5d762
Vishal-digi360 <112608428+Vishal-digi360@users.noreply.github.com>
2023-07-29 15:43:51 +0530

    okkk

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import { CarouselWithContent } from './components/Carousels'
-import Navbar from './components/Navbar'
-import Section2 from './components/Section2'
-import ImgSlider from './components/ImgSlider'
-import Article from './components/Article'
-import Cosmocorpi from './components/Cosmocorpi'
-import ImgSection from './components/ImgSection'
-import Contatti from './components/Contatti'
+import React from "react";
+import { CarouselWithContent } from "./components/Carousels";
+import Navbar from "./components/Navbar";
+import Section2 from "./components/Section2";
+import ImgSlider from "./components/ImgSlider";
+import Article from "./components/Article";
+import Cosmocorpi from "./components/Cosmocorpi";
+import ImgSection from "./components/ImgSection";
+import Contatti from "./components/Contatti";
+import HeroAnimation from "./components/HeroAnima/HeroAnimation";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(!show);
+    }, 9000);
+  }, []);
+
   return (
-    <div className='pb-96' >
-     
-      <CarouselWithContent/>
-      <Section2/>
-      <ImgSlider/>
-      
-      <Article/>
-      <Cosmocorpi/>
-      <ImgSection/>
-      <Contatti/>
-      <Navbar/>
-      
-     
+    <div className="pb-20">
+      {!show && (
+        <>
+          <HeroAnimation />
+        </>
+      )}
+
+      {show && (
+        <>
+          <CarouselWithContent />
+          <Section2 />
+          <ImgSlider />
+
+          <Article />
+          <Cosmocorpi />
+          <ImgSection />
+          <Contatti />
+          <Navbar />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HeroAnima/HeroAnimation.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const HeroAnimation = () => {
+  const [show, setShow] = useState(0);
+  const [opacity, setOpacity] = useState(" opacity-0 ");
+  const [scale, setScale] = useState(" opacity-0 ");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOpacity(" opacity-5 ");
+      setScale(" scale-95 ");
+    }, 2000);
+
+    setTimeout(() => {
+      setShow(1);
+      setOpacity(" opacity-10 ");
+      setScale(" scale-95 ");
+    }, 3000);
+    setTimeout(() => {
+      setShow(2);
+      setOpacity(" opacity-20 ");
+      setScale(" scale-90 ");
+    }, 4000);
+
+    setTimeout(() => {
+      setShow(3);
+      setOpacity(" opacity-25 ");
+      setScale(" scale-75 ");
+    }, 5000);
+    setTimeout(() => {
+      setShow(3);
+      setOpacity(" opacity-40 ");
+    }, 7000);
+    setTimeout(() => {
+      setShow(3);
+      setOpacity(" opacity-100  ");
+    }, 8000);
+  }, []);
+
+  return (
+    <>
+      <div className="h-screen overflow-hidden    ">
+        <div className="flex  justify-center items-center ">
+          <img
+            className={` ${opacity}  duration-900 ${scale} transition-opacity`}
+            src="http://archiviomariorusso.it/img/loader-bg.jpg"
+            alt=""
+          />
+        </div>
+        {console.log(show)}
+        <div className="absolute w-full top-[50%]  ">
+          <div className="flex items-center justify-center ">
+            <div className="text-white text-[25px] sm:text-[55px] text-center flex flex-col items-center justify-center Playfair-font ">
+              {show >= 1 && (
+                <>
+                  <h1 data-aos="flip-up">
+                    Ho cercato di scavare fino all’impossibile,
+                  </h1>
+                </>
+              )}
+
+              {show >= 2 && (
+                <>
+                  <h2 data-aos="flip-up">
+                    di andare all’origine, <span>ai primordi.</span>
+                  </h2>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroAnimation;
